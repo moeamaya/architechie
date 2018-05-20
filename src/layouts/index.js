a8959312bba108d9b6d88b99898d29e660de3e96
Moe Amaya <moeamaya@gmail.com>
2018-05-20 09:22:39 -0700

    Remove product from CMS

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,7 +7,7 @@ import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Helmet title="Architechie — Architecture and Technology" />
     <Navbar />
     <div>{children()}</div>
   </div>
