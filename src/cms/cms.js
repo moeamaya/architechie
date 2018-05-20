061919054812b564727acb105358972cbf568ca6
Moe Amaya <moeamaya@gmail.com>
2018-05-20 09:37:29 -0700

    Add team page to CMS

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,8 +1,10 @@
 import CMS from 'netlify-cms'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
+import TeamPagePreview from './preview-templates/TeamPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 
 CMS.registerPreviewStyle('/styles.css')
 CMS.registerPreviewTemplate('about', AboutPagePreview)
+CMS.registerPreviewTemplate('team', TeamPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
---
+++ src/cms/preview-templates/TeamPagePreview.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { TeamPageTemplate } from '../../templates/team-page'
+
+const TeamPagePreview = ({ entry, widgetFor }) => (
+  <TeamPageTemplate
+    title={entry.getIn(['data', 'title'])}
+    content={widgetFor('body')}
+  />
+)
+
+export default TeamPagePreview
